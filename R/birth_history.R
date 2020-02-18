90fac0e642acb93a5560e595f24e2b3aa86d1350
Avi Kenny <avi@lastmilehealth.org>
2020-02-18 12:35:34 -0800

    First draft of birth_history

--- R/birth_history.R
+++ R/birth_history.R
@@ -8,6 +8,19 @@
 
 birth_history <- function(mother_age) {
   
+  # CMC helper functions
+  # !!!!! Consider moving these elsewhere
+  dates_to_cmc <- function(year, month) {
+    12*(year-1900)+month
+  }
+  cmc_to_dates <- function(cmc) {
+    month <- ifelse(mod(cmc, 12)!=0, mod(cmc, 12), 12)
+    return(list(
+      year = 1900 + (cmc-month)/12,
+      month = month
+    ))
+  }
+  
   # Specify baseline mortality curve
   # Currently a linear decrease from start_year to end_year
   # Year must be between
@@ -21,8 +34,60 @@ birth_history <- function(mother_age) {
     
   }
   
-  baseline_mortality(year=2010)
+  # Loop through mother's ages to generate births
+  # !!!!! Need more accurate birth probabilities as a function of mother's age and current year
+  birthdates_cmc <- c()
+  for (i in 13:(mother_age-1)) {
+    
+    current_year <- 2023 - mother_age + i
+    birth_prob <- 1/6 # !!!!! Placeholder
+    
+    if (runif(1)<birth_prob) {
+      birthdate <- dates_to_cmc(year=current_year, month=sample(1:12,1))
+      birthdates_cmc <- c(birthdates_cmc, birthdate)
+    }
+    
+  }
   
-  # ...
+  # Loop through births to generate deaths
+  # death_prob will ignore over-five deaths
+  # !!!!! Need more accurate death probabilities as a function of child's age and current year
+  # !!!!! Death probability in time period 2022-2023 should also account for birth month
+  # !!!!! Assume for simplicity that entire survey is done in Jan 2023
+  alive <- c()
+  deathdates_cmc <- c()
+  for (i in 1:length(birthdates_cmc)) {
+    
+    alive[i] <- 1
+    deathdates_cmc[i] <- NA
+    birthdate_cmc <- birthdates_cmc[i]
+    child_birth_year <- cmc_to_dates(birthdate_cmc)$year
+    
+    for (j in 1:5) {
+      
+      if (alive[i] == 1 & (child_birth_year + j)<=2023) {
+        
+        current_year <- child_birth_year + j
+        print(paste("child is",j,"in year",current_year))
+        death_prob <- baseline_mortality(current_year)/(5*1000) # !!!!! Placeholder
+        
+        if (runif(1)<death_prob) {
+          alive[i] <- 0
+          deathdate <- dates_to_cmc(year=current_year, month=sample(1:12,1))
+          deathdates_cmc[i] <- deathdate
+        }
+        
+      }
+      
+    }
+    
+  }
+  
+  
+  return(list(
+    birthdates_cmc = birthdates_cmc,
+    alive = alive,
+    deathdates_cmc = deathdates_cmc
+  ))
   
 }
